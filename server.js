1d8927bb593845353ac92e50757643af90894ffb
Dave Trost <davetrost@gmail.com>
2019-09-10 21:03:22 -0700

    request handlers wired up

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-function onRequest(request, response) {
-    console.log('request received');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World!');
-    response.end();
+function start(route, handle) {
+    function onRequest(request, response) {
+        const pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received.`);
+
+        route(pathname, handle);
+
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.write('Hello World!');
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);
+    console.log('server has started.');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('server has started');
+exports.start = start;
